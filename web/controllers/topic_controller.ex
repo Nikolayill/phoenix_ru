68773605748dcd1fc1bb4428ee6c81f50b33a981
my7h <ustoev@front.ru>
2019-06-15 22:44:04 +0300

    #069-070 модель

--- web/controllers/topic_controller.ex
+++ web/controllers/topic_controller.ex
@@ -2,10 +2,6 @@ defmodule Discuss.TopicController do
   use Discuss.Web, :controller
 
   def new(conn, params) do
-    IO.puts "Discuss.TopicController new"
-    IO.puts "\tconn:"
-    IO.inspect conn
-    IO.puts "\tparams:"
-    IO.inspect params
+    conn
   end
 end
