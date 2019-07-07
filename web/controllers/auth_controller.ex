106697d620d3cc6375a6f666bf26c716514b8514
my7h <ustoev@front.ru>
2019-07-07 23:58:26 +0300

    # 100 Многоуровневый матчинг параметра для доступа к полю в conn

--- web/controllers/auth_controller.ex
+++ web/controllers/auth_controller.ex
@@ -2,9 +2,9 @@ defmodule Discuss.AuthController do
   use Discuss.Web, :controller
   plug Ueberauth
 
-  def callback(conn, params) do
-    IO.puts("callback conn")
-    IO.inspect(conn.assigns)
+  def callback(%{assigns: %{ueberauth_auth: auth}} = conn, params) do
+    IO.puts("callback conn.assigns.auth")
+    IO.inspect(auth)
     IO.puts("callback params")
     IO.inspect(params)
     IO.puts("callback end")
