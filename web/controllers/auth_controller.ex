206ac6df537850a5036d7f1e688983c37ed68df6
my7h <ustoev@front.ru>
2019-07-08 03:57:41 +0300

    # 101 conn -> User

--- web/controllers/auth_controller.ex
+++ web/controllers/auth_controller.ex
@@ -2,11 +2,14 @@ defmodule Discuss.AuthController do
   use Discuss.Web, :controller
   plug Ueberauth
 
+  alias Discuss.User
+
   def callback(%{assigns: %{ueberauth_auth: auth}} = conn, params) do
-    IO.puts("callback conn.assigns.auth")
-    IO.inspect(auth)
-    IO.puts("callback params")
-    IO.inspect(params)
-    IO.puts("callback end")
+    user_params = %{
+      token: auth.credentials.token,
+      email: auth.info.email,
+      provider: "github"}
+    changeset = User.changeset(%User{}, user_params)
+
   end
 end
