d53e0075e81fa4cca1c6c9bd83a043d0013d6f57
my7h <ustoev@front.ru>
2019-08-30 09:00:40 +0300

    # 137-139 Получение новых комментариев

--- web/static/js/socket.js
+++ web/static/js/socket.js
@@ -6,11 +6,17 @@ socket.connect();
 
 
 function renderComments(comments) {
-    const renderedComments = comments.map(comment => {
-        return `<li class="collection-item">${comment.content}</li>`;
-    });
-
+    const renderedComments = comments.map(commentTemplate);
     document.querySelector(".collection").innerHTML = renderedComments.join("\n");
+}
+
+function renderComment(event) {
+    const renderedComment = commentTemplate(event.comment);
+    document.querySelector(".collection").innerHTML += (renderedComment+"\n");
+}
+
+function commentTemplate(comment) {
+    return `<li class="collection-item">${comment.content}</li>`;
 }
 
 const createSocket = (topicId) => {
@@ -24,6 +30,7 @@ const createSocket = (topicId) => {
             console.log("Unable to join", resp)
         });
 
+    channel.on(`comments:${topicId}:new`, renderComment);
 
     document.querySelector("button").addEventListener("click", () => {
             const content = document.querySelector("textarea").value;
